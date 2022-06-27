4193d050b0a372e15c66bbc1267ddc35c1b5347e
StraNn1k7643 <107756482+StraNn1k7643@users.noreply.github.com>
2022-06-27 22:45:24 +0300

    Examples005_if

---
+++ Example005_if/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+String username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура ето же МАША!");
+}
+else    
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+    
