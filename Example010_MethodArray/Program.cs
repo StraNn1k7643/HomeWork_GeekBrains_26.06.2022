0fcd646fe59d6b3d59bdf512474d1138330041ab
StraNn1k7643 <107756482+StraNn1k7643@users.noreply.github.com>
2022-06-27 23:13:27 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {16,23,4,83,52,61,77,83};
+
+int n = array.Length;
+int find = 83;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
